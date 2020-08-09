f080bc352358f241d648e101da140595a54436ad
Patrick Drechsler <patrick@pdrechsler.de>
2020-08-09 23:49:54 +0200

    initial commit

---
+++ DemoTests/Tests.fs
@@ -0,0 +1,28 @@
+module Tests
+
+open System
+open Xunit
+
+type MyResult = AResult of string | OtherResult of string
+
+let someStringValidation s =
+    if String.IsNullOrWhiteSpace(s) then None
+    else
+        if s.StartsWith("a") then AResult s |> Some
+        else OtherResult s |> Some
+        
+let prompt () = Console.ReadLine()
+
+let rec getUserInputRecursive () =
+    match prompt () |> someStringValidation with
+    | None -> getUserInputRecursive ()
+    | Some x -> Some x
+
+let getUserInput = getUserInputRecursive ()
+
+[<Fact>]
+let ``User enters Space, then Tab, then letter, returns AResult abc`` () =
+    let inputs = [" "; "    "; "abc"]
+    // ???
+    // result -> pattern match success -> content should equal "AResult abc" 
+    Assert.True(true)
